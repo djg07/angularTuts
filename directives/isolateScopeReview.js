79b18c5e600d242a2ab45e4e52e91a2374597931
djg07 <d.galati54@gmail.com>
2014-12-19 12:24:59 -0500

    Message app

--- directives/isolateScopeReview.js
+++ directives/isolateScopeReview.js
@@ -1,24 +1,37 @@
-/**
- * Created by asus on 19/12/2014.
- */
 
 var app = angular.module("phoneApp", [])
 
-angular.controller("AppCtrl", function($scope) {
-    $scope.leaveVoicemail = '';
+app.controller("AppCtrl", function($scope) {
+    $scope.getDate = function() {
+        return new Date();
+    }
+    $scope.messageLog = [];
+
+    $scope.addToMessageLog = function(number, message) {
+
+        //alert(number + " " + message)
+        //alert(message);
+        $scope.messageLog.push({content:message, date: new Date()});
+        for (var x = 0; x < $scope.messageLog.length; x++) {
+            console.log($scope.messageLog[x]);
+
+        }
+    }
 
 })
 
 
-angular.directive("phone", function() {
+app.directive("phone", function() {
     return {
+        restrict: "E",
         scope: {
             number: "@",
             network: "=",
             makeCall: "&"
         },
-        template: ''
-
+        template: '<input type="text" ng-model="value" style="width:350px;">' +
+        '<div class="button" ng-click="makeCall({number:number, message:value})">Call {{number}} and leave a message</div>',
+        link: function(scope, element, attrs) {
+        }
     }
-
 })
